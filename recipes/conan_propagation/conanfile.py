b0102d754a66a9a02b492bc820d665014ff29f8d
T2\david.palat <david.palat@tescan.com>
2025-08-07 12:02:57 +0200

    config: cleanup the handling for bin/lib folders, adjust cmake

--- recipes/conan_propagation/conanfile.py
+++ recipes/conan_propagation/conanfile.py
@@ -49,51 +49,41 @@ class PropagateConan(ConanFile):
         cmake.build()
 
     def package(self):
-        build_type_folder = os.path.join(self.build_folder, str(self.settings.build_type))
+        # Copy header
         copy(
             self,
-            "propagate.h",
+            pattern="propagate.h",
             dst=os.path.join(self.package_folder, "include"),
             src=os.path.join(self.source_folder, "include"),
         )
-        copy(
-            self,
-            "*.dll",
-            dst=os.path.join(self.package_folder, "bin"),
-            src=build_type_folder,
-            keep_path=False,
-        )
-        copy(
-            self,
-            "*.lib",
-            dst=os.path.join(self.package_folder, "lib"),
-            src=build_type_folder,
-            keep_path=False,
-        )
-        copy(
-            self,
-            "*.pdb",
-            dst=os.path.join(self.package_folder, "bin"),
-            src=build_type_folder,
-            keep_path=False,
-        )
+
+        # MSVC build output folder
+        build_type_folder = os.path.join(self.build_folder, str(self.settings.build_type))
+
+        # Copy library artifacts
+        copy(self, "*.lib", dst=os.path.join(self.package_folder, "lib"), src=build_type_folder, keep_path=False)
+        #copy(self, "*.exp", dst=os.path.join(self.package_folder, "lib"), src=build_type_folder, keep_path=False)
+
+        # Copy runtime artifacts
+        copy(self, "*.dll", dst=os.path.join(self.package_folder, "bin"), src=build_type_folder, keep_path=False)
+        copy(self, "*.pdb", dst=os.path.join(self.package_folder, "bin"), src=build_type_folder, keep_path=False)
+
+        # Copy executable only if enabled
         if self.options.build_executable:
-            copy(
-                self,
-                "propagate_exec*",
-                dst=os.path.join(self.package_folder, "bin"),
-                src=build_type_folder,
-                keep_path=False,
-            )
+            copy(self, "propagate_exec.exe", dst=os.path.join(self.package_folder, "bin"), src=build_type_folder, keep_path=False)
+
+        # Generate CMake helper
         self._create_cmake_module_variables(
             os.path.join(self.package_folder, self._module_file_rel_path)
         )
+
+
 
     def _create_cmake_module_variables(self, module_file):
         content = textwrap.dedent(
             f"""\
             set(PROPAGATE_FOUND TRUE)
-            set(PROPAGATE_EXECUTABLE "${{CMAKE_CURRENT_LIST_DIR}}/../../bin/propagate_exec.exe")
+            set(PROPAGATE_EXECUTABLE "${{CMAKE_CURRENT_LIST_DIR}}/../../../bin/propagate_exec.exe")
             set(PROPAGATE_VERSION "{self.version}")
         """
         )
@@ -101,7 +91,7 @@ class PropagateConan(ConanFile):
 
     @property
     def _module_file_rel_path(self):
-        return os.path.join("lib", "cmake", "conan-official-propagate-variables.cmake")
+        return os.path.join("lib", "cmake", self.name, "propagate.cmake")
 
     def package_info(self):
         self.cpp_info.set_property("cmake_find_mode", "both")
